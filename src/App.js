cf228a3db67943a1433e173f605a8ddf83bfffb2
Paras Arya <82272875+ParasArya289@users.noreply.github.com>
2023-09-27 23:30:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import {FiChevronRight} from "react-icons/fi";
+
+export default function App() {
+  return (
+    <div className="App">
+     <button type="button">Get started<span><FiChevronRight/></span></button>
+    </div>
+  );
+}
